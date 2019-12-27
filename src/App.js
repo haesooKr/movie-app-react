b916925ffa727bcc0a33ce403a7fd9de98a34528
haesooKr <gotn676@gmail.com>
2019-12-27 01:48:23 -0500

    Created renderMovies Function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
-  }
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  renderMovies = (movies) => {
+    return movies.map(movie => (
+      <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+      />
+    ));
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getMovies();
   }
-  render(){
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Rendering Movies..." : "Movies"}</div>
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>{isLoading ? "Rendering Movies..." : this.renderMovies(movies)}</div>
+    );
   }
 }
-
 
 export default App;
