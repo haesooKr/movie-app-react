4d4089eecf0d62597821ae51f05e859a8c962e98
haesooKr <gotn676@gmail.com>
2019-12-27 02:27:41 -0500

    Styling Movie Boxes

--- src/Movie.js
+++ src/Movie.js
@@ -8,12 +8,12 @@ export default function Movie({year, title, summary, poster, genres}) {
             <div className="movie_data">
                 <h3 className="movie_title">{title}</h3>
                 <h5 className="movie_year">{year}</h5>
-                <p className="movie_summary">{summary}</p>
                 <ul className="genres">
                     {genres.map((genre, index) => (
                     <li key={index} className="genres_genre">{genre}</li>
                     ))}
                 </ul>
+                <p className="movie_summary">{summary}</p>
             </div>
         </div>
     )
