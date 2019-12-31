3658a4999b25edfc7379000e8c466a1a372b6290
haesooKr <gotn676@gmail.com>
2019-12-31 15:32:53 -0500

    Add Navigation to Router

---
+++ src/components/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function Navigation(){
+    return <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+    </div>
+}
